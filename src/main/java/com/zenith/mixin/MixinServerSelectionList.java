17e141b900af3ee8a5b5df9f2ab81263d9053bcb
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-01-10 21:03:46 -0800

    move to right side of server list gui

--- src/main/java/com/zenith/mixin/MixinServerSelectionList.java
+++ src/main/java/com/zenith/mixin/MixinServerSelectionList.java
@@ -23,7 +23,14 @@ import java.util.Map;
 @Mixin(ServerSelectionList.class)
 public abstract class MixinServerSelectionList extends ObjectSelectionList<ServerSelectionList.Entry> {
 
+    @Unique private static final ResourceLocation COOKIE_KEY_TRANSFER_SRC = ResourceLocation.tryBuild("zenith", "zenith-transfer-src");
+    @Unique private static final ResourceLocation COOKIE_KEY_SPECTATOR = ResourceLocation.tryBuild("zenith", "zenith-spectator");
     @Final @Shadow private JoinMultiplayerScreen screen;
+    @Unique final int buttonWidth = 32;
+    @Unique final int buttonHeight = 32;
+    // empty space in the left of the sprite png
+    // the sprite is 32x32, but the actual button is 20x32
+    @Unique private static final int SPRITE_LEFT_OFFSET = 12;
 
     public MixinServerSelectionList(final Minecraft minecraft, final int i, final int j, final int k, final int l) {
         super(minecraft, i, j, k, l);
@@ -31,7 +38,7 @@ public abstract class MixinServerSelectionList extends ObjectSelectionList<Serve
 
     @Override
     public boolean mouseClicked(final double mouseX, final double mouseY, final int button) {
-        if (handleMouseClick(mouseX, mouseY, button)) {
+        if (button == 0 && handleMouseClick(mouseX, mouseY, button)) {
             return true;
         }
         return super.mouseClicked(mouseX, mouseY, button);
@@ -41,6 +48,7 @@ public abstract class MixinServerSelectionList extends ObjectSelectionList<Serve
     private boolean handleMouseClick(final double mouseX, final double mouseY, final int button) {
         List<ServerSelectionList.Entry> children = this.children();
         int left = this.getRowLeft();
+        int width = this.getRowWidth();
 
         for (int i = 0; i < children.size(); i++) {
             ServerSelectionList.Entry entry = children.get(i);
@@ -52,11 +60,15 @@ public abstract class MixinServerSelectionList extends ObjectSelectionList<Serve
             String serverVersionStr = serverData.version.tryCollapseToString();
             if (serverVersionStr == null) return false;
             if (!serverVersionStr.startsWith("ZenithProxy")) continue;
-            boolean zHovering = mouseX >= left - 35 && mouseX <= left - 3 && mouseY >= top && mouseY <= top + 32;
+            int buttonBoundLeft = getButtonBoundLeft(left, width);
+            int buttonBoundRight = getButtonBoundRight(left, width);
+            int buttonBoundTop = getButtonBoundTop(top);
+            int buttonBoundBottom = getButtonBoundBottom(top);
+            boolean zHovering = isHoveringOverButton((int) mouseX, (int) mouseY, buttonBoundLeft, buttonBoundRight, buttonBoundTop, buttonBoundBottom);
             if (!zHovering) continue;
             Map<ResourceLocation, byte[]> cookies = new HashMap<>();
-            cookies.put(ResourceLocation.tryBuild("zenith", "zenith-transfer-src"), serverData.ip.getBytes(StandardCharsets.UTF_8));
-            cookies.put(ResourceLocation.tryBuild("zenith", "zenith-spectator"), String.valueOf(true).getBytes(StandardCharsets.UTF_8));
+            cookies.put(COOKIE_KEY_TRANSFER_SRC, serverData.ip.getBytes(StandardCharsets.UTF_8));
+            cookies.put(COOKIE_KEY_SPECTATOR, String.valueOf(true).getBytes(StandardCharsets.UTF_8));
             TransferState transferState = new TransferState(cookies);
             ConnectScreen.startConnecting(this.screen, this.minecraft, ServerAddress.parseString(serverData.ip), serverData, false, transferState);
             return true;
@@ -66,8 +78,10 @@ public abstract class MixinServerSelectionList extends ObjectSelectionList<Serve
 
     @Override
     public void renderWidget(final GuiGraphics guiGraphics, final int mouseX, final int mouseY, final float partialTick) {
+        super.renderWidget(guiGraphics, mouseX, mouseY, partialTick);
         List<ServerSelectionList.Entry> children = this.children();
         int left = this.getRowLeft();
+        int width = this.getRowWidth();
 
         for (int i = 0; i < children.size(); i++) {
             ServerSelectionList.Entry entry = children.get(i);
@@ -79,12 +93,44 @@ public abstract class MixinServerSelectionList extends ObjectSelectionList<Serve
             String serverVersionStr = serverData.version.tryCollapseToString();
             if (serverVersionStr == null) return;
             if (!serverVersionStr.startsWith("ZenithProxy")) continue;
-            boolean zHovering = mouseX >= left - 35 && mouseX <= left - 3 && mouseY >= top && mouseY <= top + 32;
+            int buttonBoundLeft = getButtonBoundLeft(left, width);
+            int buttonBoundRight = getButtonBoundRight(left, width);
+            int buttonBoundTop = getButtonBoundTop(top);
+            int buttonBoundBottom = getButtonBoundBottom(top);
+            boolean zHovering = isHoveringOverButton(mouseX, mouseY, buttonBoundLeft, buttonBoundRight, buttonBoundTop, buttonBoundBottom);
             guiGraphics.blitSprite(zHovering
                                        ? ServerSelectionList.JOIN_HIGHLIGHTED_SPRITE
                                        : ServerSelectionList.JOIN_SPRITE,
-                                   left - 35, top, 32, 32);
+                                   buttonBoundLeft, buttonBoundTop, buttonWidth, buttonHeight);
         }
-        super.renderWidget(guiGraphics, mouseX, mouseY, partialTick);
+    }
+
+    @Unique
+    private boolean isHoveringOverButton(final int mouseX, final int mouseY, final int buttonBoundLeft, final int buttonBoundRight, final int buttonBoundTop, final int buttonBoundBottom) {
+        return mouseX >= (buttonBoundLeft + SPRITE_LEFT_OFFSET) && mouseX <= buttonBoundRight && mouseY >= buttonBoundTop && mouseY <= buttonBoundBottom;
+    }
+
+    @Unique
+    private int getButtonBoundTop(final int top) {
+        return top;
+    }
+
+    @Unique
+    private int getButtonBoundBottom(final int top) {
+        return top + buttonHeight;
+    }
+
+    @Unique
+    private int getButtonBoundRight(final int left, final int width) {
+        return left + width + buttonWidth
+            + 6 // scrollbar width
+            - 3; // padding
+    }
+
+    @Unique
+    private int getButtonBoundLeft(final int left, final int width) {
+        return left + width
+            + 6 // scrollbar width
+            - 3; // padding
     }
 }
